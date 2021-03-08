854211a3fae701402698f3086686af625cc670a9
Mussil <79862052+Mussil@users.noreply.github.com>
2021-03-08 13:07:36 +0200

    try HTML from website

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ const express = require('express')
 const port    = process.env.PORT || 3000
 const app     = express()
 
+
+const path    = require('path')
+
 console.log(process.env.PORT)
 app.get('/', (req, res) => {
-    res.send('hello world')
+    //res.send('hello world')
+    res.sendFile(path.join(__dirname, 'index.html'))
+
 })
 
-app.get('/profile', (req, res) => {
-    res.send('welcome to profile page')
-})
+
+// app.get('/profile', (req, res) => {
+//     res.send('welcome to profile page')
+// })
 
 app.listen(port, ()=>{
     console.log(`server is up and running at: http://127.0.0.1:${port}`)
