255dabae119331fa7cfc4b149d8d41ff7436bae9
ruthie <ruticohen770@gmail.com>
2021-03-10 14:42:21 +0200

    update test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,13 @@
-module.exports =
-    {
-        'env':           {
-            'browser':  true,
-            'commonjs': true,
-            'es6':      true,
-            'node':     true,
-        },
-        'extends':       'eslint:recommended',
-        'globals':       {
-            'Atomics':           'readonly',
-            'SharedArrayBuffer': 'readonly',
-        },
-        'parserOptions': {
-            'ecmaVersion': 2018,
-        },
-        'rules':         {
-            'quotes': [
-                'error',
-                'single',
-            ],
-            'semi':   [
-                'error',
-                'never',
-            ],
-        },
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
     }
+};
