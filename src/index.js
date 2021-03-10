92277d3a1e6ac24e7131a393e35b36a5e686072a
Mussil <79862052+Mussil@users.noreply.github.com>
2021-03-10 15:42:51 +0200

    step back

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const port    = process.env.PORT || 3000
 const app     = express()
 
-
 const path    = require('path')
 
-console.log(process.env.PORT)
+//set up template engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./'));
+
+
+
 app.get('/', (req, res) => {
     //res.send('hello world')
     res.sendFile(path.join(__dirname, 'index.html'))
@@ -13,10 +19,7 @@ app.get('/', (req, res) => {
 })
 
 
-// app.get('/profile', (req, res) => {
-//     res.send('welcome to profile page')
-// })
-
+//listen to port
 app.listen(port, ()=>{
     console.log(`server is up and running at: http://127.0.0.1:${port}`)
 })
