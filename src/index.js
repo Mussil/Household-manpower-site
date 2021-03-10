65e1eba6832a828f78f3e25d078015741914814f
Mussil <79862052+Mussil@users.noreply.github.com>
2021-03-10 19:23:39 +0200

    lets start

--- src/index.js
+++ src/index.js
@@ -2,21 +2,34 @@ const express = require('express')
 const port    = process.env.PORT || 3000
 const app     = express()
 
-const path    = require('path')
+//var path = require('path')
+
 
 //set up template engine
 app.set('view engine', 'ejs')
 
 //static files
-app.use(express.static('./'))
+app.use(express.static('./views'))
+
 
 
 
 app.get('/', (req, res) => {
-    //res.send('hello world')
-    res.sendFile(path.join(__dirname, 'index.html'))
+    //res.sendFile(path.join(__dirname + './public/404.html'))
+    res.render('home')
 
 })
+
+app.get('/login', (req, res) => {
+    //es.sendFile(path.join(__dirname + '/login.html'))
+    res.render('login')
+
+})
+app.get('*', (req, res) => {
+    //res.sendFile(path.join(__dirname + '/404.html'))
+    res.render('404')
+})
+
 
 
 //listen to port
