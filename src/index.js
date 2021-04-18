9263559cf59b312d51aba3d38158797a009a5e80
Mussil <79862052+Mussil@users.noreply.github.com>
2021-04-18 22:23:15 +0300

    add loginHR, routes, new model UsersHR , home page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ const app     = express()
 //connect to mongodb
 const mongoose= require('mongoose')
 //const Users=require('../models/users')
+const authRoutes= require('../routes/auth')
 
+//static files
+app.use(express.static('./views'))
+app.use(express.json())
+
+//set up template engine
+app.set('view engine', 'ejs')
+
+
+//database connection
 const dbURI= 'mongodb+srv://user:user@cluster0.p5zoq.mongodb.net/project-db?retryWrites=true&w=majority'
 mongoose.connect(dbURI, {useNewUrlParser: true , useUnifiedTopology: true})
     .then(()=>//listen to port
@@ -14,27 +24,32 @@ mongoose.connect(dbURI, {useNewUrlParser: true , useUnifiedTopology: true})
         }))
     .catch((err)=>console.log(err))
 
-//controllers
-var loginController=require('../controllers/loginController')
-var signupController=require('../controllers/signupController')
 
 
-//set up template engine
-app.set('view engine', 'ejs')
-
-//static files
-app.use(express.static('./views'))
-
-//fire controllers
-loginController(app)
-signupController(app)
+//routes
 
 //homepage page
 app.get('/', (req, res) => {
-    //res.sendFile(path.join(__dirname + './public/404.html'))
     res.render('home')
-
 })
+
+app.use(authRoutes)
+
+
+
+//controllers
+//var loginHRController=require('../controllers/loginHRController')
+// var loginController=require('../controllers/loginController')
+// var signupController=require('../controllers/signupController')
+
+
+
+//fire controllers
+//loginHRController(app)
+// loginController(app)
+// signupController(app)
+
+
 
 
 
@@ -47,14 +62,9 @@ app.get('/success', (req, res) => {
 
 
 
+
+
 //for all the false routes
 app.get('*', (req, res) => {
-    //res.sendFile(path.join(__dirname + '/404.html'))
     res.render('404')
 })
-
-
-// //listen to port
-// app.listen(port, ()=>{
-//     console.log(`server is up and running at: http://127.0.0.1:${port}`)
-// })
