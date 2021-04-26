348ffe6d9f10a70e74578efb88bfcd397a44e44f
Mussil <79862052+Mussil@users.noreply.github.com>
2021-04-26 22:08:24 +0300

    Merge branch 'main' of https://github.com/Mussil/Project2
     Conflicts:
    	controllers/ContractorController.js

--- controllers/ContractorController.js
+++ controllers/ContractorController.js
@@ -7,16 +7,24 @@ module.exports.homepageContractorGet=(req,res)=>{
     res.render('homepageContractor')
 }
 
+module.exports.workHistoryContractorGet=(req,res)=>{
+    res.render('workHistoryContractor')
+}
+
+module.exports.salaryDetailsContractorGet=(req,res)=>{
+    res.render('salaryDetailsContractor')
+}
+
+module.exports.profileContractorGet=(req,res)=>{
+    res.render('profileContractor')
+}
+
 
 module.exports.leavePeriodContractorGet=(req,res)=>{
     res.render('leavePeriodContractor')
 }
 
 module.exports.leavePeriodContractorPost=(req,res)=>{
-
-
-
-
 
 
     console.log('here in server')
