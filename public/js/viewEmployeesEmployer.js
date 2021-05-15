e668237ed21cec132f8d8d17b74f8f330642e1b1
Mussil <79862052+Mussil@users.noreply.github.com>
2021-05-15 21:17:51 +0300

    filter by city

--- public/js/viewEmployeesEmployer.js
+++ public/js/viewEmployeesEmployer.js
@@ -143,3 +143,114 @@ function filterBudget(){
     })
 
 }
+
+const citiesArray =['eilat',
+    'jerusalem',
+    'nazareth' ,
+    'haifa',
+    'zikhron ya’akov ',
+    'arad',
+    'netanya',
+    'nahariya',
+    'kfar tavor',
+    'hebron',
+    'kinneret moshava',
+    'holon',
+    'bat shlomo',
+    'bnei brak' ,
+    'the negev desert ',
+    'the lowlands',
+    'abu snan',
+    'julis',
+    'maghar',
+    'samia',
+    'ein al-assad',
+    'isfiya',
+    'keysarya' ,
+    'beer sheva',
+    'sde boker',
+    'yad moredechai',
+    'rosh-pina' ,
+    'kiryat shmona',
+    'kiryat malachi',
+    'ein gedi',
+    'ein-hod' ,
+    ' kfar nakhum',
+    ' hazor haglilit',
+    'metula' ,
+    'harduf',
+    'ashdod',
+    'kfar kama',
+    'abu gosh' ,
+    'jezreel valley',
+    ' the judean desert',
+    'sea of galilee',
+    'beit jann',
+    'sajur',
+    'rameh',
+    'daliyat el-carmel',
+    'the golan heights',
+    'tiberias',
+    'mitspe ramon',
+    'akko',
+    'katzerin',
+    'nahalal',
+    'kfar yasif',
+    'hanita',
+    'maalot tarshiha',
+    'bat yam',
+    'the hula valley' ,
+    'hevel habsor',
+    'the arava',
+    'the galilee',
+    'gush dan',
+    'the nitzana region',
+    'yanuh',
+    'yarka',
+    'kisra',
+    'hurfeish',
+    'shefa-amr',
+    'the dead sea',
+    'tel aviv jaffa',
+    'tsefat' ,
+    'tel-hai ',
+    'ramla',
+    'hertzeliya' ,
+    'gush halav',
+    'ashkelon' ,
+    'beit shean' ,
+    'cana' ,
+    'jatt',
+    'rehaniya',
+    'degania']
+function selectOne() {
+    var select = document.getElementById('city')
+    for (var i=citiesArray.length-1; i>=0; i--) {
+        var x= citiesArray[i].charAt(0).toUpperCase() + citiesArray[i].slice(1)
+        select.options[select.options.length] = new Option(x, citiesArray[i])
+    }
+}
+selectOne()
+
+
+const citiesForm = document.querySelector('.city')
+citiesForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const city = citiesForm.city.value
+    console.log(city)
+    data.forEach(con=>{
+        var element
+
+        if (con.address.city==city){
+            console.log('fd')
+            element= document.getElementById(con._id)
+            element.style.display='block'
+
+        }
+        else{
+            element= document.getElementById(con._id)
+            element.style.display='none'
+        }
+    })
+
+})
