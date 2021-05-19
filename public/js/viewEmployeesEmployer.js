4f31c3350c6725a7aa9a2a4d57a668550d524570
Mussil <79862052+Mussil@users.noreply.github.com>
2021-05-19 11:33:01 +0300

    fix in route of viewEmployeesEmployer insted of viewEmployeesContractor

--- public/js/viewEmployeesEmployer.js
+++ public/js/viewEmployeesEmployer.js
@@ -485,3 +485,26 @@ function getIdReturnRating(exId){
     var el=data.find(el => el._id ==exId)
     return el.rating
 }
+
+
+
+//Get the button:
+var mybutton = document.getElementById('top')
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = 'block'
+    } else {
+         mybutton.style.display = 'none'
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+// eslint-disable-next-line no-unused-vars
+function topFunction() {
+    document.body.scrollTop = 0 // For Safari
+    document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+}
