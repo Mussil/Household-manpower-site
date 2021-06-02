3e47405020d5e7bed714f830e1e836b4f780da34
Shaked1 <shaked14782@gmail.com>
2021-06-02 15:47:05 +0300

    robot

--- public/js/loginEmployer.js
+++ public/js/loginEmployer.js
@@ -28,10 +28,42 @@
     passwordError.textContent = data.errors.password
 }
     if (data.user) { //successful
-    location.assign('/homepageEmployer')
-}
+        const errCaptcha = document.getElementById("errCaptcha");
+        const reCaptcha = document.getElementById("reCaptcha");
+        recaptcha = reCaptcha.value;
+        let validateCaptcha = 0;
+        for (var z = 0; z < 6; z++) {
+            if (recaptcha.charAt(z) != captcha[z]) {
+                validateCaptcha++;
+            }
+        }
+        if (recaptcha == "") {
+            errCaptcha.innerHTML = "Re-Captcha must be filled";
+        } else if (validateCaptcha > 0 || recaptcha.length > 6) {
+            errCaptcha.innerHTML = "Wrong captcha";
+            return
+        } else {
+            errCaptcha.innerHTML = "Done";
+            location.assign('/homepageEmployer')
+        }
+    }
 }
     catch (err) {
     console.log(err)
 }
 })
+
+    let captcha = new Array();
+
+    function createCaptcha() {
+        const activeCaptcha = document.getElementById("captcha");
+        for (q = 0; q < 6; q++) {
+            if (q % 2 == 0) {
+                captcha[q] = String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+            } else {
+                captcha[q] = Math.floor(Math.random() * 10 + 0);
+            }
+        }
+        theCaptcha = captcha.join("");
+        activeCaptcha.innerHTML = `${theCaptcha}`;
+    }
