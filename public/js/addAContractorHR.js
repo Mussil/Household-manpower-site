f49ba7baa60155adcad254740e6df9078830fa2f
Mussil <79862052+Mussil@users.noreply.github.com>
2021-06-02 22:53:53 +0300

    add a contractor swal

--- public/js/addAContractorHR.js
+++ public/js/addAContractorHR.js
@@ -163,6 +163,7 @@
                 var msg='A new contractor worker: '+data.user.email+' is added to the system an email on its way to him/her.'
                 // sucNew.textContent = msg
                 form.reset()
+                // eslint-disable-next-line no-undef
                 swal(msg)
 
                 //res.send('<h3>YOU ARE ADD A NEW EMPLOYER TO THE SYSTEM</h3>')
